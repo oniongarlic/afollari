8495cbcec7ec3fb3adad5fa177bf5176d98f71b6
Kaj-Michael Lang <milang@tal.org>
2018-08-20 13:24:17 +0300

    Top like output

--- afollari.adb
+++ afollari.adb
@@ -10,6 +10,10 @@ with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
 
+with Ada.Calendar; use Ada.Calendar;
+with Ada.Calendar.Formatting; use Ada.Calendar.Formatting;
+with Ada.Calendar.Conversions; use Ada.Calendar.Conversions;
+
 procedure AFollari is
  FoliData : JSON_Value := Create_Object;
  JSBody : Unbounded_String;
@@ -17,6 +21,7 @@ procedure AFollari is
  RacksTotal: Integer;
  BikesAvail: Integer;
  BikesTotal: Integer;
+ Updated: Integer;
 
  type Rack is record
   StopCode : Unbounded_String;
@@ -48,32 +53,62 @@ procedure AFollari is
   return 100.0-(Float(Avail)/Float(Total))*100.0;
  end BikeLoad;
 
+ procedure ClearScreen is
+ begin
+  Put(ASCII.ESC & "[1;1H" & ASCII.ESC & "[2J");
+ end ClearScreen;
+
+ procedure PrintData is
+ begin
+  ClearScreen;
+
+  Put("Updated");
+  Put(Updated);
+  New_Line(1);
+
+  Put("Total racks:");
+  Put(RacksTotal);
+  New_Line(1);
+
+  Put("Total bikes available:");
+  Put(BikesAvail);
+  New_Line(1);
+
+  Put("Bike Load (%):");
+  Put(BikeLoad(BikesAvail, BikesTotal), 3, 2, 0);
+  New_Line(2);
+
+  Put_Line("ID  Bikes Location");
+
+  if FoliData.Has_Field("racks") then
+   Racks:=FoliData.Get("racks");
+   Map_JSON_Object(Racks, RackCB'Access);
+  end if;
+
+  New_Line(1);
+ end PrintData;
+
+ procedure LoadData is
+  Tmp: Long_Integer;
+ begin
+  JSBody:=AWS.Response.Message_Body(AWS.Client.Get(URL => "http://data.foli.fi/citybike"));
+  FoliData:=Read(JSBody);
+
+  RacksTotal:=FoliData.Get("racks_total");
+  BikesAvail:=FoliData.Get("bikes_total_avail");
+  BikesTotal:=300;
+
+  Updated:=FoliData.Get("lastupdate");
+
+  -- XXX: How to convert from Integer ???
+  -- Updated:=To_Ada_Time();
+ end LoadData;
+
 begin
- JSBody:=AWS.Response.Message_Body(AWS.Client.Get(URL => "http://data.foli.fi/citybike"));
- FoliData:=Read(JSBody);
-
- RacksTotal:=FoliData.Get("racks_total");
- BikesAvail:=FoliData.Get("bikes_total_avail");
- BikesTotal:=300;
-
- Put("Total racks:");
- Put(RacksTotal);
- New_Line(1);
-
- Put("Total bikes available:");
- Put(BikesAvail);
- New_Line(1);
-
- Put("Bike Load (%):");
- Put(BikeLoad(BikesAvail, BikesTotal), 3, 2, 0);
- New_Line(1);
-
- Put_Line("ID  Bikes Location");
-
- if FoliData.Has_Field("racks") then
-  Racks:=FoliData.Get("racks");
-  Map_JSON_Object(Racks, RackCB'Access);
- end if;
-
- New_Line(1);
+ loop
+  LoadData;
+  PrintData;
+  delay Duration(5.0);
+  New_Line(1);
+ end loop;
 end AFollari;
